9583e43de3be289b1f4a490bfe427b36a3d7e95c
Beau Si <bs655@cornell.edu>
2020-07-12 23:33:49 -0500

    Added comments and documentation

--- ball.py
+++ ball.py
@@ -1,44 +1,103 @@
 import constants
 import pygame
 import random
-# class for the pong ball
+
 class Ball():
-    # creates a ball that starts in the middle of screen
+    """
+    A class used to represent a ball
+
+    Attributes
+    ----------
+    x: int
+        Location of the ball along the x axis
+    y: int
+        Location of the ball along the y axis
+    DX: int
+        Change in x location. Equivalent to speed along x axis
+    DY: int
+        Change in y location. Equivalent to speed along y axis
+    RADIUS: int
+        The constant radius of the ball
+
+    Methods
+    -------
+    reset()
+        Resets all attributes of the ball to their default settings
+    getRandSpeed() -> int
+        Returns a random speed value between 2 and 4 for x and y axes
+        or between -4 and -2
+    setBallPos(x: int, y:int)
+        Sets the x and y position of the ball
+    getBallPos() -> (int, int)
+        Returns the x and y position of the ball
+    render(screen: Pygame object)
+        Updates the drawing of the circle on the screen
+    """
+
     def __init__(self):
+        """
+        Creates a ball that starts in the middle of screen
+        """
         self.x = int(constants.SCREEN_WIDTH/2)
         self.y = int(constants.SCREEN_HEIGHT/2)
         self.DX = self.getRandSpeed()
         self.DY = self.getRandSpeed()
         self.RADIUS = 5
 
-    #resets all properties of the ball
     def reset(self):
+        """"
+        Resets all properties of the ball
+        """
         self.x = int(constants.SCREEN_WIDTH/2)
         self.y = int(constants.SCREEN_HEIGHT/2)
         self.DX = self.getRandSpeed()
         self.DY = self.getRandSpeed()
 
-    # get a random velocity
-    # if random number is 0, set it to something else
-    def getRandSpeed(self):
+    def getRandSpeed(self) -> int:
+        """
+        Returns a random speed between 2 and 4, or -4 and -2.
+        """
         num = int(random.uniform(-4,4))
         while(-1<=num and num<=1):
             num = int(random.uniform(-4,4))
         return num
 
-    # set (x,y) position of the ball
-    def setBallPos(self,x,y):
-        #assert x>=0 and x<=constants.SCREEN_WIDTH
-        #assert y>=0 and y<=constants.SCREEN_HEIGHT
+    def setBallPos(self, x: int, y: int):
+        """
+        Sets the (x,y) position of the ball
+
+        Parameters
+        ----------
+        x: int
+            Desired x location of where to place the ball
+        y: int
+            Desired y location of where to place the ball
+        """
         self.x = x
         self.y = y
 
-    # get the (x,y) position of the ball
-    def getBallPos(self):
+    def getBallPos(self) -> (int,int):
+        """
+        Get the (x,y) position of the ball
+
+        Returns
+        -------
+        (x location of ball, y location of ball)
+        """
         return self.x, self.y
 
     # updates the screen rendering of the ball
     # pong.py will call the pygame update display method once to reduce computation
     def render(self, screen):
+        """
+        Updates the drawing of hte ball on the screen
+        Pong.py will call the pygame updateDisplay method one time
+        to reduce computation
+
+        Parameters
+        ----------
+        screen: pygame object
+            The Pygame screen
+        """
         x,y = self.getBallPos()
         pygame.draw.circle(screen, (255, 255, 255), (x, y), self.RADIUS)
--- player.py
+++ player.py
@@ -1,8 +1,49 @@
 import constants
 import pygame
-# creates a player object i.e. a rectangular paddle
+
 class Player():
+    """
+    A class used to represent a player.
+    Each player is represented as a rectangular paddle.
+
+    Attributes
+    ----------
+    WIDTH: int
+        Constant width of the paddle
+    HEIGHT: int
+        Constant height of the paddle
+    SPEED: int
+        Constant vertical speed of the paddle
+    left: int
+        X axis position of the rectangle's top left corner
+    top: int
+        Y axis position of the rectangle's top left corner
+    score: int
+        Number of points a player has
+
+
+    Methods
+    -------
+    setPlayerPos(left: int, top: int)
+        Sets the player paddle's position on the screen
+    getPlayerPos() -> (int, int)
+        Returns the top left coordinates of the paddle
+    setScore(score: int)
+        Sets the score for the player
+    getScore() -> int
+        Returns the player's score
+    updatePlayerGivenKeypress(leftSide: bool)
+        Updates the specified player's paddle position given keyboard input
+    render(screen: Pygame object)
+        Updates the drawing of the player paddle on the screen
+    """
+
     def __init__(self):
+        """
+        Creates a rectangular paddle for a player.
+        Paddle dimensions and speed are fixed.
+        Paddle placement initialized later for the players.
+        """
         self.WIDTH = 10 #paddle width
         self.HEIGHT = 100 #paddle height
         self.SPEED = 10 #how fast paddle can move
@@ -12,30 +53,70 @@ class Player():
         self.top = -100
         self.score = 0
 
-    # sets the location of the rectangular paddles
-    def setPlayerPos(self,left,top):
+    def setPlayerPos(self, left: int, top: int):
+        """
+        Sets the location of the rectangular paddle.
+        Ensures that the paddles are placed on the screen.
+
+        Parameters
+        ----------
+        left: int
+            X axis position of the rectangle's top left corner
+        top: int
+            Y axis position of the rectangle's top left corner
+        """
         assert left==0 or left==(constants.SCREEN_WIDTH-self.WIDTH)
         assert top >=0 and top <= constants.SCREEN_HEIGHT-self.HEIGHT
         self.left = left
         self.top = top
 
-    # gets the top left coordinate of the rectangle
-    def getPlayerPos(self):
+    def getPlayerPos(self) -> (int, int):
+        """
+        Gets the top left coordinate of the rectangle
+
+        Returns
+        -------
+        (Rectangle's top left corner's X coordinate,
+        Rectangle's top left corner's Y coordinate)
+        """
         return self.left,self.top
 
-    # sets players score
-    def setScore(self,score):
+
+    def setScore(self, score: int):
+        """
+        Sets player's score
+
+        Parameters
+        ----------
+        score: int
+            Player's score for the game
+        """
         assert score >= 0
         self.score = score
 
-    # get the player score
-    def getScore(self):
+    def getScore(self) -> int:
+        """
+        Get the player's score
+
+        Returns
+        -------
+        Game score of the player
+        """
         return self.score
 
-    # update the position of the player given key input
-    # takes boolean input leftSide, which determines player
-    # position on screen as well as player controls
-    def updatePlayerGivenKeypress(self, leftSide):
+
+    def updatePlayerGivenKeypress(self, leftSide: bool):
+        """
+        Update the position of the player given keyboard input.
+        Keys "W" and "S" move Player 1 up and down on the left half of the screen.
+        Up and down arrows move Player 2 up and down on the right half of the screen.
+
+        Parameters
+        ---------
+        leftSide: bool
+            True for setting controls of Player 1 on the left side of the screen.
+            False for setting controls of Player 2 on the right side of the screen.
+        """
         left,top = self.getPlayerPos()
         keys = pygame.key.get_pressed()
         # for the left side player
@@ -52,8 +133,15 @@ class Player():
                 top = min(top + self.SPEED, constants.SCREEN_HEIGHT - self.HEIGHT)
         self.setPlayerPos(left,top)
 
-    # updates the screen rendering of the player
-    # pong.py will call the pygame update display method once to reduce computation
     def render(self, screen):
+        """
+        Updates the drawing of the player paddle on the screen.
+        pong.py will call the pygame update display method once to reduce computation
+
+        Parameters
+        ----------
+        screen: Pygame object
+            The Pygame screen the game itself is played on.
+        """
         left, top = self.getPlayerPos()
         pygame.draw.rect(screen, (255, 255, 255), (left, top, self.WIDTH, self.HEIGHT))
